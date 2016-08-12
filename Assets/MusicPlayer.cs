a9c9e64cf7ec2e05f9d020bb357190fbced1f698
Roger Guldbrandsen <roger@kinbiko.com>
2016-08-12 22:49:12 +0100

    Renamed music file to bgm, and created music player.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    //Singleton to ensure restarting the scene doesn't cause a new music player.
+    static MusicPlayer instance = null;
+
+
+    //Doing singleton management in Awake rather than start to ensure
+    //that a second music player won't be heard for a split second when
+    //starting the game
+    void Awake ()
+    {
+        if(instance != null){
+            Destroy(gameObject);   
+        } else {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+    void Start () {}
+	void Update () {}
+}
