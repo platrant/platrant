9e40755031d0a41ebb83f74b45a5478901276e54
Daniel Hilburn <tagg8945@aol.com>
2016-08-12 18:38:44 -0500

    fixed all code quality issues - I think

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-	private Rigidbody2D rgBody;
-	[SerializeField]
-	private float movementSpeed;
-	[SerializeField]
-	private Transform[] groundedPoints;
-	void Start ()
-	{
-		rgBody = GetComponent<Rigidbody2D>();
-	}
-	void FixedUpdate ()
-	{
-		float horizontal = Input.GetAxis("Horizontal");
-		Debug.Log(horizontal);
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rgBody;
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private Transform[] groundedPoints;
+    void Start()
+    {
+        rgBody = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        MovementHandler(horizontal);
+    }
+    private void MovementHandler(float horizontal)
+    {
+        rgBody.velocity = new Vector2(horizontal * movementSpeed, rgBody.velocity.y);
 
-		movementHandler(horizontal);
-	}
-	private void movementHandler(float horizontal)
-	{
-		rgBody.velocity = new Vector2(horizontal * movementSpeed, rgBody.velocity.y);
-
-		shouldJump(horizontal);
-	}
-	private void shouldJump(float horizontal)
-	{
-		if (Input.GetButtonDown("Jump") && rgBody.velocity.y == 0)
-		{
-			rgBody.velocity = new Vector2(horizontal * movementSpeed, 3);
-		}
-	}
+        ShouldJump(horizontal);
+    }
+    private void ShouldJump(float horizontal)
+    {
+        if (Input.GetButtonDown("Jump") && rgBody.velocity.y == 0)
+        {
+            rgBody.velocity = new Vector2(horizontal * movementSpeed, 3);
+        }
+    }
 }
