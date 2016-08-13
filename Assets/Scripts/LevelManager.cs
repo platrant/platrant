e380e692247a1dd2def44e8ed0ef120151f5ff2a
Roger Guldbrandsen <roger@kinbiko.com>
2016-08-13 15:41:18 +0100

    Made LevelManager and added splash screen to build settings

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    public void RequestQuit()
+    {
+        Application.Quit();
+    }
+}
