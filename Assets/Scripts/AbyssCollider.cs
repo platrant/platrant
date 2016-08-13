7b14da609f6d68febf6478c1e52567d9ab68c463
Roger Guldbrandsen <roger@kinbiko.com>
2016-08-13 21:10:35 +0100

    Created an abyss in Level_01

---
+++ Assets/Scripts/AbyssCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbyssCollider : MonoBehaviour {
+
+    [SerializeField]
+    private LevelManager levelManager;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("In on trigger enter");
+        levelManager.RequestQuit();
+    }
+
+	void Start () { }
+	void Update () { }
+}
