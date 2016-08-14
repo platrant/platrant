f09fef80505b51bdf68fbe71ebb9b155a0b771ae
lealtagaming@gmail.com <lealtagaming@gmail.com>
2016-08-14 13:59:52 +0100

    Enemy movement
    Added enemy movement and tweaked the collider layers so we can use
    waypoints

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private int moveDirection = 1;
+    private int rotation = 180;
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveDirection, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.CompareTag("EnemyWayPoint"))
+        {
+            moveDirection = -moveDirection;
+            gameObject.transform.Rotate(0, 180, 0);
+        }
+    }
+}
