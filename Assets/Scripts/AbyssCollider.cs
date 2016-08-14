7f76308100a748a10bc2cb1f6130949df6d04d72
lealtagaming@gmail.com <lealtagaming@gmail.com>
2016-08-14 20:50:38 +0100

    Clean up from review
    Removed unneeded code per PR review

--- Assets/Scripts/AbyssCollider.cs
+++ Assets/Scripts/AbyssCollider.cs
@@ -4,19 +4,10 @@ using System.Collections;
 public class AbyssCollider : MonoBehaviour {
 
     private const string PLAYER_TAG = "Player";
-
-    [SerializeField]
-    private LevelManager levelManager;
-    [SerializeField]
-    private PlayerMovement player;
-
+    
     void OnTriggerExit2D(Collider2D coll)
     {
         if (coll.gameObject.tag == PLAYER_TAG)
             coll.gameObject.SendMessage("LoseLife");
-    }
-
-    void Start () {
-        
     }
 }
