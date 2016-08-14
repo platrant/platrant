61f33ae9837ec4964dfac16bf7915f94bcdbbef3
Roger Guldbrandsen <roger@kinbiko.com>
2016-08-14 14:57:24 +0100

    Added a PlayerHealthManager and font-awesome

--- Assets/Scripts/AbyssCollider.cs
+++ Assets/Scripts/AbyssCollider.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class AbyssCollider : MonoBehaviour {
 
+    public const string PLAYER_TAG = "Player";
+    
+    [SerializeField]
+    private PlayerHealthManager healthManager;
+
     [SerializeField]
     private LevelManager levelManager;
 
-    void OnTriggerEnter2D(Collider2D collider)
+    void OnTriggerEnter2D(Collider2D coll)
     {
-        Debug.Log("In on trigger enter");
-        levelManager.RequestQuit();
+        if (coll.gameObject.tag == PLAYER_TAG)
+        {
+            healthManager.LoseLife();
+            Destroy(coll.gameObject);
+            UpdateLivesComponent(healthManager.GetRemainingLives());
+        }
+    }
+
+    private void UpdateLivesComponent(int remainingLives)
+    {
+        char fontAwesomeHealthSymbol = '-';//'\uf004;';
+        UpdateRemainingLivesText(new string(fontAwesomeHealthSymbol, remainingLives));
+    }
+
+    private void UpdateRemainingLivesText(string newText)
+    {
+        GameObject.Find("Remaining Lives").GetComponent<UnityEngine.UI.Text>().text = newText;
     }
 
 	void Start () { }
---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthManager : MonoBehaviour {
+
+    int remainingLives = 3;
+
+    public void LoseLife()
+    {
+        remainingLives--;
+        CheckIfAlive();
+    }
+
+    public int GetRemainingLives()
+    {
+        return remainingLives;
+    }
+
+    private void CheckIfAlive()
+    {
+        if(remainingLives <= 0)
+        {
+            Debug.Log("you daed");
+        }
+    }
+
+	void Start() {}
+	void Update() {}
+}
