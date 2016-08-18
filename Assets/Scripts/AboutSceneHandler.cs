7fcb85ec39f7505dd1dc689b840d060eec11e4af
Trey50Daniel <tagg8945@gmail.com>
2016-08-18 11:11:58 -0500

    Updated about scene to go back to main menu

---
+++ Assets/Scripts/AboutSceneHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AboutSceneHandler : MonoBehaviour {
+	
+	void Update () {
+	    if(Input.GetKey("escape"))
+        {
+            ExtendedSceneManager.LoadScene(SceneName.Main_Menu);
+        }
+	}
+}
