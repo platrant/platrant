27ad631f842d6ffec344e62e038f770fe79adf8d
Roger Guldbrandsen <roger@kinbiko.com>
2016-08-19 21:27:42 +0100

    Almost back to original LEVEL_01, only remains to solve odd gravity jump bug

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,21 +2,22 @@
 
 public class EnemyController : MonoBehaviour {
 
-    private int moveDirection = 1;
+    [SerializeField]
+    private int movementSpeed = 1;
 
     [SerializeField]
     private PlayerHealthHandler playerHealthHandler;
 
     void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(moveDirection, 0);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.CompareTag(TagName.EnemyWayPoint))
         {
-            moveDirection = -moveDirection;
+            movementSpeed= -movementSpeed;
             gameObject.transform.Rotate(0, 180, 0);
         }
         else if (coll.gameObject.CompareTag(TagName.Player))
