59934ce79217ff7993914378338756b0a1646a67
Trey50Daniel <tagg8945@gmail.com>
2016-08-19 13:59:24 -0500

    Made game restart at main menu after 5 seconds

---
+++ Assets/Scripts/GameRestartHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRestartHandler : MonoBehaviour
+{
+
+    private float time = 0;
+    void Start()
+    {
+        time = 5;
+    }
+
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            ExtendedSceneManager.LoadScene(SceneName.Main_Menu);
+        }
+    }
+}
