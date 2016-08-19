51dc62b0356d494e358da12df21bfef11176859f
Trey50Daniel <tagg8945@gmail.com>
2016-08-19 09:01:15 -0500

    Normalized line endings in AboutSceneHandler

--- Assets/Scripts/AboutSceneHandler.cs
+++ Assets/Scripts/AboutSceneHandler.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AboutSceneHandler : MonoBehaviour {
-	
-	void Update () {
-	    if(Input.GetKey("escape"))
+public class AboutSceneHandler : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
         {
             ExtendedSceneManager.LoadScene(SceneName.Main_Menu);
         }
-	}
+    }
 }
